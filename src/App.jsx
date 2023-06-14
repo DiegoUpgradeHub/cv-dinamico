105c3b36b631faec44963cba48e1244d2021b305
DiegoUpgradeHub <diego.perez@bootcamp-upgrade.com>
2023-06-14 22:41:27 +0100

    Nuevos componentes instalados, creación de la página Portfolio, primeras pruebas usando Material UI

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 //Components imported
 import Home from "./pages/Home.jsx";
 import Contact from "./pages/Contact.jsx";
+import Portfolio from "./pages/Portfolio.jsx";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/contact" element={<Contact />} />
+          <Route exact path="/portfolio" element={<Portfolio />} />
       </Routes>
     </div>
   );
